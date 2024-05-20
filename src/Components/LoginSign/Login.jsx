61c04252bc6a26e17f83a6ec7d54de5fa80586c5
AzizAzmiJ-A-I <sukiastoria1945@gmail.com>
2024-05-20 23:08:10 +0700

    add Dashboard page

--- src/Components/LoginSign/Login.jsx
+++ src/Components/LoginSign/Login.jsx
@@ -38,7 +38,7 @@ const Login = () => {
     };
 
     return (
-        <div className='container'>
+        <div className="container">
             <div className="header">
                 <div className="text">Login</div>
                 <div className="underline"></div>
---
+++ src/Components/Main/Menu.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import './Menu.css';
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import LockIcon from '@mui/icons-material/Lock';
+
+function Menu(){
+    return (
+        <div>
+            <div className="containerMenu">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Menu
