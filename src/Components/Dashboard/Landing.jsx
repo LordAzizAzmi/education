8469c088ac5d696b77b2615f7f096c14337224e8
AzizAzmiJ-A-I <sukiastoria1945@gmail.com>
2024-07-23 20:42:51 +0700

    upload poster

--- src/Components/Dashboard/Landing.jsx
+++ src/Components/Dashboard/Landing.jsx
@@ -1,15 +1,18 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import './Dashboard.css';
-import Logo from '../Assets/logoAMA.jpg';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Dashboard.css";
+import Logo from "../Assets/logoAMA.jpg";
+import Poster from "../Assets/poster.jpg";
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
 
 function Landingpage() {
-
   useEffect(() => {
     // Hapus sesi local storage
-    localStorage.removeItem('username');
-    localStorage.removeItem('userId');
-    localStorage.removeItem('lastActive');
+    localStorage.removeItem("username");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("lastActive");
   }, []);
 
   return (
@@ -22,26 +25,24 @@ function Landingpage() {
           </div>
         </div>
         <br />
-        <div className="title">" AMA " <br /> EDUCATION CENTRE
+        <div className="title">
+          " AMA " <br /> EDUCATION CENTRE
         </div>
         <br />
-        <div className="carddashboard">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path>
-          </svg>
-          <div className="carddashboard__content">
-            <img src="new_image.png" alt="Logo" />
-            <p className="carddashboard__title">AMA EDUCATION CENTRE</p>
-            <p className="carddashboard__description">POSTER</p>
-          </div>
-        </div>
-
+        <br />
+        <br />
+        <Col xs={6} md={4}>
+          <Image src={Poster} thumbnail />
+        </Col>
+        <br />
+        <br />
         <Link to="/Login">
           <button className="buttonLGSG" style={{ top: "90%", left: "50%" }}>
             Masuk
           </button>
         </Link>
-        <br /><br />
+        <br />
+        <br />
         <Link to="/Register">
           <button className="buttonLGSG" style={{ top: "90%", left: "50%" }}>
             Daftar
